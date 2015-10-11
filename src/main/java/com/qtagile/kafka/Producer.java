13079a1c28adeade020f4d1713ffb7c13d38b32a
mtraina <matteo.traina.mail@gmail.com>
2015-10-11 22:01:19 +0100

    added gradle properties and message producer

---
+++ src/main/java/com/qtagile/kafka/Producer.java
@@ -0,0 +1,32 @@
+package com.qtagile.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class Producer {
+    private final KafkaProducer<String,String> producer;
+
+    public Producer(int serverPort){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, String.format("localhost:%d", serverPort));
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        producer = new KafkaProducer<String,String>(props);
+    }
+
+    public Future<RecordMetadata> send(String topic, String key, String value){
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<String,String>(topic, key, value);
+        return producer.send(producerRecord);
+    }
+
+    public void shutdown(){
+        producer.close();
+    }
+}
